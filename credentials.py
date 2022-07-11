19094160c09d8817a78a332e3ab90189de346ed9
MittMatt77 <93163453+MittMatt77@users.noreply.github.com>
2022-07-11 08:38:04 -0400

    Updated account info

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = "Your Account Sid"
-auth_token = "Your Auth Token"
-my_cell = "+Your Cell Number"
-my_twilio = "+Your Twilio Number"
+account_sid = "ACb95c95938b555213158d8bbb15dade85"
+auth_token = "07a8e1d5d615433cee54d4628d0be6a1"
+my_cell = "+8036913878"
+my_twilio = "+19705508591"
